c238ffde4e904ccc03d0ddc4cc0cb7e7826715de
Paul FICOT <paul.ficot@gmail.com>
2021-05-11 12:34:29 +0200

    Get post list & post by id

---
+++ sql/get_post_by_id.sql
@@ -0,0 +1,23 @@
+SELECT
+     posts.post_id
+    ,posts.title
+    ,posts.user_id
+    ,users.username
+    ,posts.content AS 'description'
+    ,views.view_count
+    ,(SELECT COUNT(like_id) FROM posts_like likes
+        WHERE likes.post_id = posts.post_id AND likes.is_like = TRUE) AS 'likes'
+    ,(SELECT COUNT(like_id) FROM posts_like dislikes
+        WHERE dislikes.post_id = posts.post_id AND dislikes.is_like = FALSE) AS 'dislikes'
+
+FROM posts
+
+INNER JOIN picture pictures ON (posts.post_id = pictures.post_id)
+INNER JOIN users ON (posts.user_id = users.user_id)
+INNER JOIN posts_view views ON (posts.post_id = views.post_id)
+
+WHERE
+    posts.post_id = 2
+
+GROUP BY
+    posts.post_id
---
+++ sql/get_post_list.sql
@@ -0,0 +1,11 @@
+SELECT
+     posts.post_id
+    ,users.username
+    ,posts.title
+    ,posts.content AS 'description'
+    ,pictures.picture_id
+    ,pictures.url
+FROM posts
+
+INNER JOIN users ON (posts.user_id = users.user_id)
+INNER JOIN picture pictures ON (posts.post_id = pictures.post_id) AND (pictures.is_thumbnail = TRUE)
