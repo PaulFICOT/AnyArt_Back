dc915a84fb59c0c10b7ddb898a00f8381697926f
Paul FICOT <paul.ficot@gmail.com>
2021-05-11 14:59:17 +0200

    Adding salt to user table

--- sql/init_db.sql
+++ sql/init_db.sql
@@ -28,6 +28,7 @@ CREATE TABLE users(
    firstname VARCHAR(100) NOT NULL,
    mail VARCHAR(255) NOT NULL,
    password VARCHAR(255) NOT NULL,
+   salt VARCHAR(8) NOT NULL,
    birth_date DATE NOT NULL,
    username VARCHAR(100) NOT NULL,
    crea_date DATETIME NOT NULL,
