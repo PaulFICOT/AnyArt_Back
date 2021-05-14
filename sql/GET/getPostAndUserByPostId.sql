c3e711cc02ac3b6abf23b69b723854bf9c349318
Paul FICOT <paul.ficot@gmail.com>
2021-05-14 16:39:35 +0200

    Adding informations to post by id query

--- sql/GET/getPostAndUserByPostId.sql
+++ sql/GET/getPostAndUserByPostId.sql
@@ -3,6 +3,11 @@ SELECT
     ,posts.title
     ,posts.user_id
     ,users.username
+    ,users.is_verified
+    ,users.job_function
+    ,users.open_to_work
+    ,picture.picture_id
+    ,picture.url
     ,posts.content
     ,views.view_count
     ,(SELECT COUNT(like_id) FROM posts_like likes
@@ -14,6 +19,7 @@ FROM posts
 
 INNER JOIN users ON (posts.user_id = users.user_id)
 INNER JOIN posts_view views ON (posts.post_id = views.post_id)
+INNER JOIN picture ON (users.user_id = picture.user_id AND picture.post_id IS NULL)
 
 WHERE
     posts.post_id = 2
